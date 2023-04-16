4d2eb0c8b29b6f3f7bf566b6a87038e8a0392796
pilipchuk-philip <pilipchuk.philip@gmail.com>
2023-04-16 23:58:01 +0200

    Final config for plugins

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,40 +1,30 @@
 vim.cmd [[packadd packer.nvim]]
 
-----------------------------------------------
--- TODO
---  \
---   - TODO
-----------------------------------------------
-
 return require('packer').startup(function()
-    -----------------------------------------------------------
-    -- Packer
-    -----------------------------------------------------------
     use 'wbthomason/packer.nvim'
-
-    -----------------------------------------------------------
-    -- Core Plugins
-    -----------------------------------------------------------
-    use 'tpope/vim-surround' -- Surround
-    use 'tpope/vim-repeat' -- Может повторять через . vimsurround
-    use 'cohama/lexima.vim' -- Закрывает автоматом скобки
-    use 'b3nj5m1n/kommentary' -- Коменты
-    use 'powerman/vim-plugin-ruscmd' -- Russian comand line expired
-    use 'MattesGroeger/vim-bookmarks' -- Bookmarks
+    ------------------
+    -- Core Plugins --
+    ------------------
+    use "tpope/vim-surround" -- Surround
+    use "tpope/vim-repeat" -- Может повторять через . vimsurround
+    use "cohama/lexima.vim" -- Закрывает автоматом скобки
+    use "b3nj5m1n/kommentary" -- Коменты
+    use "powerman/vim-plugin-ruscmd" -- Russian comand line expired
+    use "MattesGroeger/vim-bookmarks" -- Bookmarks
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0', -- Search
+        "nvim-telescope/telescope.nvim", tag = '0.1.0', -- Search
         requires = {
-            'nvim-lua/popup.nvim',
-            'nvim-telescope/telescope-media-files.nvim'
+            "nvim-lua/popup.nvim",
+            "nvim-telescope/telescope-media-files.nvim"
         }
     }
-    use {'kkoomen/vim-doge', run = ':call doge#install()'} -- generate doc
-    -----------------------------------------------------------
-    -- CODE
-    -----------------------------------------------------------
+    use {"kkoomen/vim-doge", run = ":call doge#install()"} -- generate doc
+    ------------------
+    -- CODE         --
+    ------------------
     use {
-        'nvim-treesitter/nvim-treesitter', -- Code Highlight
-        run = ':TSUpdate'
+        "nvim-treesitter/nvim-treesitter", -- Code Highlight
+        run = ":TSUpdate"
     }
     use({
         "hrsh7th/nvim-cmp",  -- Autocomplete
@@ -55,54 +45,54 @@ return require('packer').startup(function()
         config = function() require('plugins.snippets.snippets') end
     }
     use "rafamadriz/friendly-snippets"
-    use 'j-hui/fidget.nvim' -- status for loading plugins
+    use "j-hui/fidget.nvim" -- status for loading plugins
     use "ray-x/lsp_signature.nvim" -- Show function signature
-    use 'neovim/nvim-lspconfig' -- Lsp config files
-    use 'williamboman/nvim-lsp-installer' -- Lsp installer
-    use 'stsewd/sphinx.nvim' -- Sphinx intergration
-    -----------------------------------------------------------
-    -- GIT
-    -----------------------------------------------------------
-    use 'mattn/webapi-vim' -- Git Gist
-    use 'mattn/vim-gist' -- Git Gist
-    use 'TimUntersberger/neogit' -- Git Client
-    use 'lewis6991/gitsigns.nvim' -- Git Blame
-    use 'airblade/vim-gitgutter' -- Git Gutter
-    use 'sindrets/diffview.nvim' -- Diff view
-    -----------------------------------------------------------
-    -- UI
-    -----------------------------------------------------------
+    use "neovim/nvim-lspconfig" -- Lsp config files
+    use "williamboman/nvim-lsp-installer" -- Lsp installer
+    use "stsewd/sphinx.nvim" -- Sphinx intergration
+    ------------------
+    -- GIT          --
+    ------------------
+    use "mattn/webapi-vim" -- Git Gist
+    use "mattn/vim-gist" -- Git Gist
+    use "TimUntersberger/neogit" -- Git Client
+    use "lewis6991/gitsigns.nvim" -- Git Blame
+    use "airblade/vim-gitgutter" -- Git Gutter
+    use "sindrets/diffview.nvim" -- Diff view
+    ------------------
+    -- UI           --
+    ------------------
     use "lukas-reineke/indent-blankline.nvim" -- Выделение отступов
-    use 'RRethy/vim-illuminate' -- Highlight Same
-    use 'folke/tokyonight.nvim' -- Theme Tokio Night
-    use 'ryanoasis/vim-devicons' -- Icons
-    use 'kyazdani42/nvim-web-devicons' -- Icons
-    use 'nvim-lua/plenary.nvim'
-    use 'preservim/nerdtree' -- NerdTree
-    use {'andymass/vim-matchup', event = 'VimEnter'} -- Подсветка парных скобок
-    use 'nvim-lualine/lualine.nvim' -- Нижняя панель
-    use 'google/vim-searchindex'  -- Считает кол-во совпадений при поиске
+    use "RRethy/vim-illuminate" -- Highlight Same
+    use "folke/tokyonight.nvim" -- Theme Tokio Night
+    use "ryanoasis/vim-devicons" -- Icons
+    use "kyazdani42/nvim-web-devicons" -- Icons
+    use "nvim-lua/plenary.nvim" -- Telescope part
+    use "preservim/nerdtree" -- NerdTree
+    use {"andymass/vim-matchup", event = 'VimEnter'} -- Подсветка парных скобок
+    use "nvim-lualine/lualine.nvim" -- Нижняя панель
+    use "google/vim-searchindex"  -- Считает кол-во совпадений при поиске
     use {
-        'gelguy/wilder.nvim',  -- Wilder
+        "gelguy/wilder.nvim",  -- Wilder
         requires = { "kyazdani42/nvim-web-devicons",},
     }
-    use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'} -- Tabs Barline
-    use 'xiyaowong/nvim-transparent' --Transporency
+    use {"romgrk/barbar.nvim", wants = 'nvim-web-devicons'} -- Tabs Barline
+    use "xiyaowong/nvim-transparent" --Transporency
     use {
         "SmiteshP/nvim-navic",    -- Navic [ Code Breadcrambs ]
         requires = "neovim/nvim-lspconfig"
     }
-    use({
-      "utilyre/barbecue.nvim",
-      requires = {
-        "neovim/nvim-lspconfig",
-        "SmiteshP/nvim-navic",
-        "nvim-tree/nvim-web-devicons", -- optional dependency
-      },
-      after = "nvim-web-devicons", -- keep this if you're using NvChad
-      config = function()
-        require("barbecue").setup()
-      end,
-    })
+    use {
+        "utilyre/barbecue.nvim",
+        requires = {
+            "neovim/nvim-lspconfig",
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons",
+        },
+        after = "nvim-web-devicons",
+        config = function()
+            require("barbecue").setup()
+        end,
+    }
 
 end)
