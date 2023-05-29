b36208be955dfb7898798453574e58e46aefdb6f
pilipchuk-philip <pilipchuk.philip@gmail.com>
2023-05-29 14:06:48 +0200

    base configs updated(removed old), split keys added

--- lua/configs/base.lua
+++ lua/configs/base.lua
@@ -1,98 +1,95 @@
--- ------------------------------------------- 
+-- -------------------------------------------
 -- Variables
 -- -------------------------------------------
-local cmd = vim.cmd             -- execute Vim commands
-local g = vim.g                 -- global variables
-local opt = vim.opt             -- global/buffer/windows-scoped options
-
--- ------------------------------------------- 
+-- -------------------------------------------
 -- Base
 -- -------------------------------------------
-g.translate_source = 'ru'
-g.translate_target = 'en'
-opt.undofile = true                 -- Возможность отката назад
-opt.splitright = true               -- vertical split вправо
-opt.splitbelow = true               -- horizontal split вниз
+vim.g.translate_source = 'ru'
+vim.g.translate_target = 'en'
 
-vim.o.title              = true
-vim.o.termguicolors      = true
+vim.opt.splitright = true               -- vertical split вправо
+vim.opt.splitbelow = true               -- horizontal split вниз
+vim.opt.title = true
+vim.opt.termguicolors = true
 
-cmd[[set noerrorbells]]
-cmd[[set nomodeline ]]
+vim.opt_global.errorbells = false -- Убрать звук ошибки
+vim.opt_global.modeline = false -- Убрать блымы экрана
 
-vim.o.showcmd            = true
-vim.o.lazyredraw         = true
-vim.o.copyindent         = true
-vim.o.showmatch          = true
+vim.opt.list = false  -- спец символы
 
+vim.opt.showcmd  = true
+vim.opt.lazyredraw  = true
+vim.opt.copyindent = true
+
+vim.cmd([[
+    filetype indent plugin on
+    syntax enable
+]])
 
 -- -------------------------------------------
 -- Tabs and Spaces
 -- -----------------------------------------------------------
-cmd([[
-filetype indent plugin on
-syntax enable
-]])
 
-opt.tabstop = 4           -- 1 tab == 4 spaces
-opt.shiftwidth = 4        -- shift 4 spaces when tab
-opt.smarttab = true
-opt.smartindent = true    -- autoindent new lines
-opt.expandtab = true      -- use spaces instead of tabs
-opt.scrolloff=4
-
+vim.opt.tabstop = 4           -- 1 tab == 4 spaces
+vim.opt.shiftwidth = 4        -- shift 4 spaces when tab
+vim.opt.smarttab = true
+vim.opt.smartindent = true    -- autoindent new lines
+vim.opt.autoindent = true    -- autoindent new lines
+vim.opt.breakindent = true    -- autoindent new lines
+vim.opt.expandtab = true      -- use spaces instead of tabs
+vim.opt.scrolloff=4
 
 -- -------------------------------------------
 -- Search
 -- -------------------------------------------
-vim.o.ignorecase = true
-vim.o.smartcase= true
-vim.o.showmode= true
-cmd[[set nowrap]]
-
-
-
--- vim.o.virtualedit=all
-vim.o.hlsearch= true
-vim.o.incsearch= true
-vim.o.gdefault= true
-cmd[[set  nolist]]
-
-
-cmd[[ set virtualedit=all ]]
-cmd[[ set hlsearch ]]
-cmd[[ set incsearch ]]
-cmd[[ set gdefault ]]
-cmd[[ set nolist ]]
-cmd[[ set mouse=a ]]
-cmd[[ set fileformats="unix,dos,mac" ]]
-cmd[[ set formatoptions+=1 ]]
-cmd[[ set autoread ]]
+vim.opt.ignorecase = true
+vim.opt.smartcase= true
+vim.opt.showmatch = true
+vim.opt.showmode= true
+vim.wo.wrap = false
+vim.opt.hlsearch= true
+vim.opt.incsearch= true
+vim.opt.gdefault= true
+vim.opt.virtualedit="all" -- Allow virtual editing in all modes
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.g.mouse = "a"
+vim.g.autoread = true
+vim.opt.fileformats ="unix,dos,mac"
+vim.opt.inccommand = "split" -- shows the effects of :substitute, :smagic, :snomagic and user commands with the :command-preview flag as you
 
 -- -------------------------------------------
 -- Tabs for Filetypes
 -- -------------------------------------------
 
-cmd [[au BufEnter * set fo-=c fo-=r fo-=o]] -- don't auto commenting new lines
-cmd [[autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0]] -- remove line lenght marker for selected filetes
-cmd [[
-autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml,htmljinja setlocal shiftwidth=4 tabstop=4
+vim.cmd [[au BufEnter * set fo-=c fo-=r fo-=o]] -- don't auto commenting new lines
+vim.cmd [[autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0]] -- remove line lenght marker for selected filetes
+vim.cmd [[
+autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml,htmljinja setlocal shiftwidth=2 tabstop=2
 ]] -- 2 spaces for selected filetypes
 
 -- -------------------------------------------
 -- Backups
 -- -------------------------------------------
-cmd [[ set nobackup ]]                   -- do not keep backup files, it's 70's style
-cmd [[ set noswapfile  ]]                -- do not write annoying intermediate swap
+vim.opt.backup = false -- do not keep backup files, it's 70's style
+vim.opt.swapfile = false -- do not write annoying intermediate swap
+vim.opt.undofile = true  -- Возможность отката назад
 
 -- -------------------------------------------
 -- Encoding
 -- -------------------------------------------
-cmd [[ set termencoding=utf-8 ]]
-cmd [[ set encoding=utf-8 ]]
-cmd [[ set fileformat=unix ]]
+vim.opt.encoding = 'utf-8'
+vim.opt.termencoding = 'uft-8'
+vim.opt.fileencoding = 'utf-8'
+vim.scriptencoding = 'utf-8'
+vim.opt.fileformat = 'unix'
 
 -- -------------------------------------------
 -- Minus Triling whitespaces
 -- -------------------------------------------
-cmd [[ autocmd BufWritePre * :%s/\s\+$//e ]]
+vim.cmd [[ autocmd BufWritePre * :%s/\s\+$//e ]]
+
+-- -------------------------------------------
+-- Shell
+-- -------------------------------------------
+vim.opt.shell = 'zsh'
--- lua/configs/keymaps/base.lua
+++ lua/configs/keymaps/base.lua
@@ -1,28 +1,44 @@
-local cmd = vim.cmd             -- execute Vim commands
-
 -----------------------------------------------------------------------
---- COMMON 
+-- COMMON
 -----------------------------------------------------------------------
-cmd [[ let mapleader = "'" ]]
+vim.cmd [[ let mapleader = "'" ]]
 -- if mac os
--- cmd [[ vmap <C-c> "*y ]]
-cmd [[ vmap <C-c> "+y ]]
+-- vim.cmd [[ vmap <C-c> "*y ]]
+vim.cmd [[ vmap <C-c> "+y ]]
 
-cmd [[ nnoremap ; : ]]
-cmd [[ nnoremap p P ]]
-cmd [[ nnoremap ; : ]]
-cmd [[ nnoremap <leader>; ;  ]]
+vim.cmd [[ nnoremap ; : ]]
+vim.cmd [[ nnoremap p P ]]
+vim.cmd [[ nnoremap <leader>; ;  ]]
 
 -----------------------------------------------------------------------
 --- NERDTRee
 -----------------------------------------------------------------------
-cmd [[ nnoremap <BS> :NERDTreeToggle<CR> ]]
+vim.cmd [[ nnoremap <BS> :NERDTreeToggle<CR> ]]
 
 -----------------------------------------------------------------------
---- TODO PYTHON
+-- Toggle show/hide invisible chars
 -----------------------------------------------------------------------
+vim.cmd [[ nnoremap <leader>i :set list!<cr>]]
 
---  Toggle show/hide invisible chars  
-cmd [[ nnoremap <leader>i :set list!<cr>]]
+-----------------------------------------------------------------------
+-- Select all
+-----------------------------------------------------------------------
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G', {silent=true})
 
+-----------------------------------------------------------------------
+-- Tabs, Splits
+-----------------------------------------------------------------------
+vim.keymap.set('n', '<C-t>', ':tabedit<Return>', {silent=true})
+vim.keymap.set('n', 'ss', ':split<Return><C-w>w', {silent=true})
+vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w', {silent=true})
+
+vim.keymap.set('n', 's<left>', '<C-w>h', {silent=true})
+vim.keymap.set('n', 's<right>', '<C-w>l', {silent=true})
+vim.keymap.set('n', 's<up>', '<C-w>k', {silent=true})
+vim.keymap.set('n', 's<down>', '<C-w>j', {silent=true})
+
+vim.keymap.set('n', 'sh', '<C-w>h', {silent=true})
+vim.keymap.set('n', 'sl', '<C-w>l', {silent=true})
+vim.keymap.set('n', 'sk', '<C-w>k', {silent=true})
+vim.keymap.set('n', 'sj', '<C-w>j', {silent=true})
 
