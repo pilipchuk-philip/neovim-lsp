0d65ee198fb76f324a0bb07c3bfb4892e5107839
pilipchuk-philip <pilipchuk.philip@gmail.com>
2024-07-07 22:04:42 +0200

    remap upadte

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,10 +3,6 @@ local tb = require('telescope.builtin')
 
 ------------------------------------------
 -- Basics
--- Map Leader
-vim.g.mapleader = ' ' -- Set <space> as the leader key
-vim.g.maplocalleader = ' '
-
 -- See `:help vim.keymap.set()`
 keymap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 keymap('n', ';', ':', { silent = true })
@@ -124,7 +120,7 @@ keymap('n', '<C-y>', ':Telescope lsp_document_symbols ignore_symbols=variable<CR
 keymap('n', '<C-t>', ':TodoTelescope<CR>')
 keymap('n', '<C-d>', ':Telescope diagnostics burfnr=0<CR>')
 
-keymap('n', '<leader>b', ':Telescope vim_bookmarks all<CR>')
+keymap('n', 'gb', ':Telescope vim_bookmarks all<CR>', { silent = true })
 keymap('n', 'gf',
   ':lua require("telescope").extensions.git_file_history.git_file_history()<CR>',
   { silent = true }
@@ -132,14 +128,12 @@ keymap('n', 'gf',
 
 -- RENAME
 keymap('n', '<leader>r', ':IncRename')
+-- CodeActions
+keymap('n', '<leader>ca', ':lua require("actions-preview").code_actions()<CR>')
 
 -- GIT
 keymap('n', '<leader>lg', ':LazyGit <CR>')
-keymap('n', '<leader>gh', ':lua package.loaded.gitsigns.preview_hunk() <CR>')
 keymap('n', '<leader>gf', ':lua require("telescope").extensions.git_file_history.git_file_history()<CR>')
-
--- CodeActions
-keymap('n', '<leader>ca', ':lua require("actions-preview").code_actions()<CR>')
 
 -- Chat
 keymap('n', '<leader>c', ':ChatGPT <CR>')
