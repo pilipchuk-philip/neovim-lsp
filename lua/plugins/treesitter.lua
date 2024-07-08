a31cc123c079cf8bd4b4493ba2869d0022ed62ae
pilipchuk-philip <pilipchuk.philip@gmail.com>
2024-07-08 17:49:12 +0200

    treesitter fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,41 +5,35 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
     'andymass/vim-matchup',
     'RRethy/vim-illuminate', -- Highlight Same
+    'nvim-treesitter/nvim-treesitter-context',
   },
   build = ':TSUpdate',
-  opts = {
-    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-    ensure_installed = {
-      "c", "python", "javascript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",
-    },
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = true,
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-    -- List of parsers to ignore installing (or "all")
-    -- ignore_install = { "javascript" },
+    configs.setup({
+      auto_install = true,
+      modules = {},
+      ignore_install = {},
+      ensure_installed = { "c", "lua", "vim", "python", "vimdoc", "query", "elixir", "heex", "javascript", "html", "sql" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
 
-    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-    highlight = {
-      enable = true,
-
-      -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-      -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-      -- the name of the parser)
-      -- list of language that will be disabled
-      disable = { "latex", },
-      disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-          return true
-        end
-      end,
-      additional_vim_regex_highlighting = false,
-    },
-  }
+    require 'treesitter-context'.setup {
+      enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20,     -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+    }
+  end,
 }
