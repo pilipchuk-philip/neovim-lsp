e4aa5088ea0cbd84f3790b1c07224cbe3cd2c42d
pylyp pylypchuk <ppy@csis.com>
2024-07-28 23:49:52 +0200

    nerdtree fix

--- lua/plugins/nerdtree.lua
+++ lua/plugins/nerdtree.lua
@@ -2,10 +2,6 @@ return {
   "preservim/nerdtree",
   config = function()
     vim.cmd [[
-      autocmd BufEnter * if winnr() == winnr('h') && bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
-      \ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
-    ]]
-    vim.cmd [[
       let NERDTreeIgnore = ['__pycache__', '\.pyc$']
     ]]
   end,
