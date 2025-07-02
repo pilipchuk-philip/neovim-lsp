6c73e675884a92f275012a86656d4b430cd490c2
Pilipchuk Philip <pilipchuk.philip@gmail.com>
2025-07-02 22:37:46 +0200

    NERDTree deleted, neotree added

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -39,18 +39,7 @@ end
 
 keymap('n', 'ff', ':lua ToggleFoldMethod() <CR>', { silent = true })
 
--- Func: Toggle explorer
---[[ function ToggleExplorer()
-  local neotree = require('neo-tree')
-  local win_id = vim.fn.bufwinid('neo-tree filesystem [1]')
-  if win_id ~= -1 then
-    vim.api.nvim_win_close(win_id, true)
-  else
-    require('neo-tree.command').execute({ action = "show", source = "filesystem", position = "left" })
-  end
-end ]]
-
-vim.keymap.set({ 'n', 'v' }, '<BS>', ':NERDTreeToggle<CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<BS>', ':Neotree toggle<CR>', { silent = true })
 ------------------------------------------
 -- Basics
 keymap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,9 +1,19 @@
 return {
-  "preservim/nerdtree",
-  config = function()
-    vim.cmd [[
-      let NERDTreeShowHidden=1
-      let NERDTreeIgnore=['\.git$', '\.idea$', '\.vscode$', '\.history$']
-    ]]
-  end
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  ---@module "neo-tree"
+  ---@type neotree.Config?
+  opts = {
+    window = {
+      -- отключает navigate_up
+      mappings = {
+        ["<bs>"] = false,
+      },
+    },
+  },
 }
